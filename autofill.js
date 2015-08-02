51050517c36d347ef9a1271e86eab5a7fff44f8c
kpavankumaar <k.pavankumaar@gmail.com>
2015-08-02 10:11:31 +0530

    getDynamic data for autofill

--- autofill.js
+++ autofill.js
@@ -1,13 +1,12 @@
-
-      var fName           = document.getElementById('fname');
-      var lName           = document.getElementById('lname');
-      var email           = document.getElementById('email');
-      var headLine        = document.getElementById('headline');
-      var NoOfConnections = document.getElementById('numConnections');
-      var siteLink        = document.getElementById('siteProfileRequest');
-      var mySummary       = document.getElementById('summary');
-      var profileImgEl    = document.getElementById('profileimage');
-      var profileImage    = document.createElement('img');
+      var fName           = document.getElementById('fname'),
+          lName           = document.getElementById('lname'),
+          email           = document.getElementById('email'),
+          headLine        = document.getElementById('headline'),
+          NoOfConnections = document.getElementById('numConnections'),
+          siteLink        = document.getElementById('siteProfileRequest'),
+          mySummary       = document.getElementById('summary'),
+          profileImgEl    = document.getElementById('profileimage'),
+          profileImage    = document.createElement('img');
 
 			var loadLinkedin = function(){
 				IN.Event.on(IN, "auth", OnLinkedInAuth);
@@ -19,48 +18,23 @@
 			          .fields(["siteStandardProfileRequest", "firstName", "lastName", "headline", "summary", "num-connections", "picture-url", 'id','email-address','date-of-birth','phone-numbers','positions'])
 			          .result(function(result) {
 			            console.log('test result ', JSON.stringify(result));
+                  bindFormData(result);
 			          });
 			}
+		
+    var bindFormData = function(profileDetails){
+      if(typeof profileDetails === 'object'){
+        console.log(profileDetails.values[0].firstName);
 
-
-			var ShowProfileData = function(profiles) {
-			    var member = profiles.values[0];
-			    var id=member.id;
-			    var firstName=member.firstName; 
-			    var lastName=member.lastName; 
-			    var photo=member.pictureUrl; 
-			    var headline=member.headline; 
-
-
-			    console.log('profiles', profiles);
-			    //use information captured above
-			}
-		var xyz    = {
-                    "_total": 1,
-                    "values": [{
-                      "_key": "~",
-                      "emailAddress": "k.pavankumaar@gmail.com",
-                      "firstName": "Pavan",
-                      "headline": "Front End Developer",
-                      "id": "Pv30EVPeqt",
-                      "lastName": "Kumar",
-                      "numConnections": 1,
-                      "pictureUrl": "https://media.licdn.com/mpr/mprx/0__ow4ipSvQevq6xhcdUl9ImuvGu8eh78o7UP46HWvie5dhxOoFH0nwwFvLEBehxuEiajMF4kzfmEW_wEb2MmXIH5JEmEH_wsIoMmRhdKqT7xkWgTBQH4v8gYLoRK4owDV_VoqbUlN_xF",
-                      "positions": {"_total": 0},
-                      "siteStandardProfileRequest": {"url": "https://www.linkedin.com/profile/view?id=241840088&authType=name&authToken=5uTv&trk=api*a4562121*s4878891*"},
-                      "summary": "Front End Developer . .."
-                    }]
-                  }
-    console.log(xyz.values[0].firstName);
-
-    /*binding */
-    fName.value            = xyz.values[0].firstName;          
-    lName.value            = xyz.values[0].lastName;                               
-    email.value            = xyz.values[0].emailAddress;                               
-    headLine.value         = xyz.values[0].headline;                               
-    NoOfConnections.value  = xyz.values[0].numConnections;                               
-    siteLink.value         = xyz.values[0].siteStandardProfileRequest.url;
-    mySummary.value        = xyz.values[0].summary;    
-    profileImage.src       = xyz.values[0].pictureUrl;    
-    profileImgEl.appendChild(profileImage);
-	
+        /*binding */
+        fName.value            = profileDetails.values[0].firstName;          
+        lName.value            = profileDetails.values[0].lastName;                               
+        email.value            = profileDetails.values[0].emailAddress;                               
+        headLine.value         = profileDetails.values[0].headline;                               
+        NoOfConnections.value  = profileDetails.values[0].numConnections;                               
+        siteLink.value         = profileDetails.values[0].siteStandardProfileRequest.url;
+        mySummary.value        = profileDetails.values[0].summary;    
+        profileImage.src       = profileDetails.values[0].pictureUrl;    
+        profileImgEl.appendChild(profileImage);
+      }
+	 }
